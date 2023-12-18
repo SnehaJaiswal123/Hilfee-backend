dc4a73785bb798289919526e000a73edfedb3247
SnehaJaiswal123 <snehajais270703@gmail.com>
2023-12-18 14:48:03 +0530

    nodemon

--- Controller/jobs.js
+++ Controller/jobs.js
@@ -1,16 +1,13 @@
-const Job = require("../model/jobs");
-const User=require('../model/user')
+const Job = require("../Model/jobs");
+const User = require("../Model/user");
 
 const cloudinary = require("cloudinary").v2;
-
 
 cloudinary.config({
   cloud_name: "dw9yboxwi",
   api_key: "955522367718116",
   api_secret: "FhTmZ4hFU4IxGm5asF8AQhxV4xQ",
 });
-
-
 
 const createJob = async (req, res) => {
   try {
@@ -26,9 +23,9 @@ const createJob = async (req, res) => {
 
 const getUserRole = async (req, res) => {
   try {
-    const owner=req.query.owner
+    const owner = req.query.owner;
     console.log(owner);
-    const user = await User.findOne({ _id:owner });
+    const user = await User.findOne({ _id: owner });
     await user.populate("roles");
     res.json(user.roles);
     console.log(user.roles);
@@ -38,16 +35,17 @@ const getUserRole = async (req, res) => {
   }
 };
 
-
 const recording = async (req, res) => {
   try {
-    const result = await cloudinary.uploader.upload_stream({ resource_type: 'video' }, (error, result) => {
-      if (error) throw error;
-      res.json({ secure_url: result.secure_url });
-    }).end(req.file.buffer);
+    const result = await cloudinary.uploader
+      .upload_stream({ resource_type: "video" }, (error, result) => {
+        if (error) throw error;
+        res.json({ secure_url: result.secure_url });
+      })
+      .end(req.file.buffer);
   } catch (error) {
-    console.error('Error uploading to Cloudinary:', error);
-    res.status(500).json({ error: 'Error uploading to Cloudinary' });
+    console.error("Error uploading to Cloudinary:", error);
+    res.status(500).json({ error: "Error uploading to Cloudinary" });
   }
 };
 
