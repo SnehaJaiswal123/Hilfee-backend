985ba19f57a4a90d6256140ef86489b70dabc4fb
SnehaJaiswal123 <snehajais270703@gmail.com>
2023-12-18 15:20:18 +0530

    added await

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose=require('mongoose')
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("db connected"))
-.catch((e)=>console.log(e))
+const mongo=async()=>{
+    await mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log("db connected"))
+    .catch((e)=>console.log(e))
+}
+mongo()
 
 module.exports=mongoose
